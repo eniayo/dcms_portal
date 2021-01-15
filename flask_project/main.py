e4acc1818c4900db2a82fe8f54caaf6c9b0eef9f
einayo_a <eniayoadediran@gmail.com>
2021-01-15 13:40:43 +0100

    initial commit

--- flask_project/main.py
+++ flask_project/main.py
@@ -1,21 +1,20 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
-
-
+    
 @app.route('/login')
 def login():
     return render_template ('login.html')
 
-
 @app.route('/register')
-def about():
+def register():
     return render_template ('register.html')
 
-
-@app.route('/')
+@app.route('/home')
 def home():
     return render_template ('home.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
